fe7bf16a633aaa7f100113a1f1c3d9e8a6a656de
EsraDemircelik <esrademircelikk@outlook.com>
2021-12-06 09:39:26 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var car in carManager.GetCarsByBrandId(2))
+            {
+                Console.WriteLine(car.Description);
+            }
+            foreach(var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ColorId + " renkli " + car.BrandId +" marka aracın " +
+                                  car.DailyPrice + " günlük fiyatılı araç" );
+            }
+        }
+    }
+}
